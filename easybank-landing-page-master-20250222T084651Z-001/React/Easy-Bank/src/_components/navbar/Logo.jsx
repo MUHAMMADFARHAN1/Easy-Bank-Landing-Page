09a059107b9cee9ab007ccad150eb595a669f35f
Muhammad Farhan <farhan_75230@hotmail.com>
2025-04-19 11:35:02 +0200

    Logo init performed

--- easybank-landing-page-master-20250222T084651Z-001/React/Easy-Bank/src/_components/navbar/Logo.jsx
+++ easybank-landing-page-master-20250222T084651Z-001/React/Easy-Bank/src/_components/navbar/Logo.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+// import logo from "./../images/logo.svg";
+import logo from "../../images/logo.svg";
 
-export default function Logo(props) {
-  return <a href="#">{props.children}</a>;
+//Importing Images in a jsx file
+//Method 1
+// import img_name from "relative path"
+// <img src={img_name} />
+// Method 2
+{
+  /* <img src="/path_starting_from_public" /> */
+}
+
+// A guide:
+// https://cloudinary.com/guides/web-performance/5-ways-to-import-images-in-react-bonus-automation-method
+
+export default function Logo() {
+  return <img src={logo} alt="image here" />;
 }
